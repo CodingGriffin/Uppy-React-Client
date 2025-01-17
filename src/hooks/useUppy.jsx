2f515bd37737cd7c4e039ed84ca68cc84ec31510
grit8525 <hawkins_roberto_untaint@outlook.com>
2025-01-17 13:24:57 -0600

    modified endpoint

--- src/hooks/useUppy.jsx
+++ src/hooks/useUppy.jsx
@@ -171,7 +171,7 @@ export function useUppy() {
       //     return response.json()
       //   },
       // })
-      .use(Tus, { endpoint: 'https://a6e7-45-144-28-239.ngrok-free.app/uploads/' })
+      .use(Tus, { endpoint: 'https://79df-45-144-28-239.ngrok-free.app/uploads/' })
   //     // .use(GoogleDrive, {
   //     //   target: Dashboard,
   //     //   companionUrl: `${endPoint}'
