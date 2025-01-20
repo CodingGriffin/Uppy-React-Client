32a150c3da936d304b8830e8bb0a5c00bf1c383e
grit8525 <hawkins_roberto_untaint@outlook.com>
2025-01-19 18:05:20 -0600

    test

--- src/hooks/useUppy.jsx
+++ src/hooks/useUppy.jsx
@@ -173,6 +173,8 @@ export function useUppy() {
       // })https://9ad1-45-144-28-239.ngrok-free.app
       .use(Tus, { 
         endpoint: 'https://7562-45-144-28-239.ngrok-free.app/uploads/',
+        removeFingerprintOnSuccess: false,
+
         // endpoint:'http://45.144.28.239:3000/uploads/'
       })
   //     // .use(GoogleDrive, {
